0a88aba7e9e05d318846afd7ba0e49082513fdbd
DilRUz <dil.atoliya@gmail.com>
2023-04-08 15:59:08 +0500

    JavaScript-ModalWindow

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 const contactUs = document.querySelector(".header-top__btn")
 const modalWindow = document.querySelector(".form")
+const btn = document.querySelector("#close")
 
 contactUs.addEventListener("click", ()=>{
     modalWindow.classList.add("open")
 })
+
+btn.addEventListener('click', ()=>{
+    modalWindow.classList.add("hidden")
+})
+
+
